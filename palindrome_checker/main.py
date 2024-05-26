1d0f9c983a28f15ac9b733c7389dd2bc0dc1c3e7
justarandomdudewithalongname <165289874+justarandomdudewithalongname@users.noreply.github.com>
2024-05-26 10:32:35 +0200

    Update main.py

--- palindrome_checker/main.py
+++ palindrome_checker/main.py
@@ -1,4 +1,17 @@
-with open('./input.txt', 'r') as f:
-  input = f.read()
+import re
 
-print(input)
+data = []
+with open('./input.txt', 'r') as f:
+    text = f.readlines()
+    for line in text:
+        word = line.lower().strip()
+        word_stripped = re.sub(r'[^\w]', '', word)
+        data.append(word_stripped)
+
+for word in data:
+    unique_chars = set(word)
+    if word == word[::-1]:
+        print('YES,', len(unique_chars))
+    else:
+        print('NO,', -1)
+
